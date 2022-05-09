b8f365209ff20fd7173c6fab51d4a8abb1b9f95f
Gal Levy <94178712+galsurf@users.noreply.github.com>
2022-05-09 22:38:26 +0300

    Update phoneBook.js
    need to fix 'fname' tag etc.

--- phoneBook.js
+++ phoneBook.js
@@ -1,13 +1,28 @@
-let advs = [];
-const adv1 = {
+contacts = [];
+    contacts[0]={
+        fName:"defaulty",
+        lName:"defaultivy",
+        phoneNumber: "0000",
+        eMailAdd:"default123@default.com"
+    }
 
-}
 
+ 
 function sorter(){
     console.log("yess");
 };
 
 
+function save(){
+    if(contacts[0].fName == "defaulty"){
+        contacts[0].fName = document.getElementById("fname");
+        contacts[0].lName = document.getElementById("lname");
+        contacts[0].phoneNumber = document.getElementById("Pnumber");
+        contacts[0].eMailAdd = document.getElementById("email"); 
+    }
+    console.log(contacts[0].fName + contacts[0].lName );
+    
+}
 
 
 
